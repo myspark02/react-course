46308b759073d5f9a3acf8af28885b117de96291
myspark02 <myspark02@gmail.com>
2022-05-25 14:14:58 +0900

    useState, useEffect

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from 'react-router-dom';
 
 function NewMeetupPage() {
+    const navigate = useNavigate();
+
+    function addMeetupHander(meetupData) {
+        fetch(
+            'https://fir-frontend-ade39-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+            {
+                method: 'POST', 
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Typoe' : 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/', {replace: true});
+        });
+    }
     return (
         <section>
             <h1> New Meetup Page</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHander}/>
         </section>
     )
 }
